8f840f3741686fbfe7878ed8a6644003aa17da13
mumodann <92576480+mumodann@users.noreply.github.com>
2023-04-04 14:04:29 +0300

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+  "https://docs.google.com/document/d/1V-xsgGU7xGYzg9CbYL8Isvu9RwXOZiJJ2wwKCRvhmIg/edit?usp=sharing";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
