88a91264e3400e1d6b1fc7100474971de830ac79
mumodann <92576480+mumodann@users.noreply.github.com>
2023-04-04 14:01:08 +0300

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -18,7 +18,7 @@ function Projects() {
           My Recent <strong className="purple">Works </strong>
         </h1>
         <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently.
+          During my internship period with  EinsBrand Digital here are a few projects I worked on:
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
